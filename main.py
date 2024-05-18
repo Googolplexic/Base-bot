b1b6e0c361eff6088e8be29468252a18572b2a48
Willywum <will.ch2705@gmail.com>
2024-05-18 16:25:48 -0700

    first command

--- main.py
+++ main.py
@@ -5,25 +5,57 @@ import os
 from dotenv import load_dotenv
 import discord
 
+from discord import Intents
+from discord.ext import commands
+from discord import app_commands
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
 
 load_dotenv()
+
+prefix = "!"
+
+
+# @client.command(pass_context=True)
+# async def getguild(ctx):
+#     guild_id = ctx.message.guild.id
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await tree.sync(guild=discord.Object(id = 1241468028378677308))
+    print("Ready!")
 
+
+
+
+# Add the guild ids in which the slash command will appear.
+# If it should be in all, remove the argument, but note that
+# it will take some time (up to an hour) to register the
+# command if it's for all guilds.
+@tree.command(
+    name="commandname",
+    description="My first application Command",
+    guild=discord.Object(id=1241468028378677308)
+)
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.lower() == (prefix + 'changeprefix'):
+        await message.channel.send('Okay! what would you like to change it to?')
+
+    if message.content.startswith( prefix + 'hello'):
         await message.channel.send('Hello!')
+
 
 
 try:
